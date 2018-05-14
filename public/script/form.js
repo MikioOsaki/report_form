f0fa1d45e37baae752729abdc6f1a1bf8daba215
mikioosaki <mikio.osaki@posteo.de>
2018-05-14 13:21:42 +0200

    Add caching for selected spot

--- public/script/form.js
+++ public/script/form.js
@@ -1,34 +1,36 @@
 jQuery(document).ready(function () {
 
 
-    var selection_bathingspot = $("#selection_bathingspot");
-    var url_string = window.location.href;
-    var url = new URL(url_string);
-    console.log(url);
-    var spot = url.searchParams.get("spot");
+/**
+ *     if (sessionStorage.getItem('spot') == null) {
+        var selection_bathingspot = $("#selection_bathingspot");
+        var url_string = window.location.href;
+        var url = new URL(url_string);
+        console.log(url);
+        var spot = url.searchParams.get("spot");
 
-    var lat = url.searchParams.get("lat");
-    var lng = url.searchParams.get("lng");
-    if (lat != null && lng != null) {
+        var lat = url.searchParams.get("lat");
+        var lng = url.searchParams.get("lng");
+        if (lat != null && lng != null) {
 
-        $("#labelPosition").text("lat: " + lat + ", lng: " + lng);
+            $("#labelPosition").text("lat: " + lat + ", lng: " + lng);
+        } else {
+            $("#labelPosition").text("Keine Position ausgewählt");
+        }
+
+        switch (spot) {
+            case "spot1":
+                selection_bathingspot.val("Option 1");
+                break;
+            case "spot2":
+                selection_bathingspot.val("Option 2");
+                break;
+            default:
+                selection_bathingspot.val("Option 4");
+                break;
+        }
     }
-    else{
-        $("#labelPosition").text("Keine Position ausgewählt");
-    }
-
-
-    switch (spot) {
-        case "spot1":
-            selection_bathingspot.val("Option 1");
-            break;
-        case "spot2":
-            selection_bathingspot.val("Option 2");
-            break;
-        default:
-            selection_bathingspot.val("Option 4");
-            break;
-    }
+ */
 
 
 });
---
+++ public/script/formcache.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function () {
+
+    var inputs = $('#uploadForm :input');
+    var spot = $('#selection_bathingspot');
+    var category = $('#selection_category');
+    var headline = $('#input_headline');
+    var text = $('#textarea');
+
+    if (sessionStorage.getItem('spot') == null) {
+        var url_string = window.location.href;
+        var url = new URL(url_string);
+        console.log(url);
+        var urlparam = url.searchParams.get("spot");
+
+        var lat = url.searchParams.get("lat");
+        var lng = url.searchParams.get("lng");
+        if (lat != null && lng != null) {
+
+            $("#labelPosition").text("lat: " + lat + ", lng: " + lng);
+        } else {
+            $("#labelPosition").text("Keine Position ausgewählt");
+        }
+
+        switch (urlparam) {
+            case "spot1":
+                spot.val("Option 1");
+                break;
+            case "spot2":
+                spot.val("Option 2");
+                break;
+            default:
+                spot.val("Option 4");
+                break;
+        }
+        sessionStorage.setItem('spot', spot.val());
+    }
+
+    if (sessionStorage.getItem('spot') != null) {
+        $(selection_bathingspot).val(sessionStorage.getItem('spot'));
+    }
+    $(spot).change(function () {
+        sessionStorage.setItem('spot', spot.val());
+    });
+    
+
+});
