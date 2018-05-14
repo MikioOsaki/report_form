58f5244db65a1e3eb244b405e074abe39733429f
mikioosaki <mikio.osaki@posteo.de>
2018-05-14 13:57:24 +0200

    Add caching for all inputs except uploaded image

--- public/script/formcache.js
+++ public/script/formcache.js
@@ -5,12 +5,14 @@ jQuery(document).ready(function () {
     var category = $('#selection_category');
     var headline = $('#input_headline');
     var text = $('#textarea');
+    var image = $('#reportImage');
+
     var url_string = window.location.href;
     var url = new URL(url_string);
     var lat = url.searchParams.get("lat");
     var lng = url.searchParams.get("lng");
-    if (lat != null && lng != null) {
 
+    if (lat != null && lng != null) {
         $("#labelPosition").text("lat: " + lat + ", lng: " + lng);
     } else {
         $("#labelPosition").text("Keine Position ausgewählt");
@@ -48,11 +50,34 @@ jQuery(document).ready(function () {
     }
 
     if (sessionStorage.getItem('spot') != null) {
-        $(selection_bathingspot).val(sessionStorage.getItem('spot'));
+        spot.val(sessionStorage.getItem('spot'));
+    }
+    if (sessionStorage.getItem('category') != null) {
+        category.val(sessionStorage.getItem('category'));
+    }
+    if (sessionStorage.getItem('headline') != null) {
+        headline.val(sessionStorage.getItem('headline'));
+    }
+    if (sessionStorage.getItem('text') != null) {
+        text.val(sessionStorage.getItem('text'));
+    }
+    if (sessionStorage.getItem('reportImage') != null) {
+        reportImage.val(sessionStorage.getItem('reportImage'));
     }
     $(spot).change(function () {
         sessionStorage.setItem('spot', spot.val());
     });
-
+    $(category).change(function () {
+        sessionStorage.setItem('category', category.val());
+    });
+    $(headline).change(function () {
+        sessionStorage.setItem('headline', headline.val());
+    });
+    $(text).change(function () {
+        sessionStorage.setItem('text', text.val());
+    });
+    $(reportImage).change(function () {
+        sessionStorage.setItem('reportImage', reportImage.val());
+    });
 
 });
