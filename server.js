656c0f75f21f52827a94178684f11555ed7a0e58
mikioosaki <mikio.osaki@posteo.de>
2018-05-15 17:20:09 +0200

    Delete unnessersary code (ext)

--- server.js
+++ server.js
@@ -70,18 +70,18 @@ app.post('/upload', function (req, res) {
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
 
-    // The name of the ---input field--- (i.e. "reportImage") is used to retrieve the uploaded file
+    // The name of the ---input field--- (i.e. "image_uploads") is used to retrieve the uploaded file
     let reportImage = req.files.image_uploads;
 
     // Use the mv() method to place the file somewhere on your server
-    if (true) {
+    if (reportImage != null) {
         reportImage.mv(__dirname + '/public/uploads/image_' + timeStamp.timeStamp(), function (err) {
             if (err)
                 return res.status(500).send(err);
             res.send('File uploaded!');
         });
     } else {
-        res.send('File extention ' + ext + " not supported. Please use jpg or png only.");
+        res.send('File not uploaded');
     }
 
 });
