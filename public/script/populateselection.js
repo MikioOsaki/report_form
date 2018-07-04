07e6ddb387aff76875d4d937e8e36f3db874f306
mikioosaki <mikio.osaki@posteo.de>
2018-07-04 14:49:52 +0200

    Simplification 2 again

--- public/script/populateselection.js
+++ public/script/populateselection.js
@@ -1,16 +1,7 @@
 jQuery(document).ready(function () {
 
-    var inputs = $('#uploadForm :input');
     var spot = $('#selection_bathingspot');
     var category = $('#selection_category');
-    var headline = $('#input_headline');
-    var textarea = $('#textarea');
-    var image_uploads = $('#image_uploads');
-    var firstname = $('#input_firstname');
-    var lastname = $('#input_lastname');
-    var email = $('#input_email');
-    var phone = $('#input_phone');
-    var checkbox_send_copy = $('#checkbox_send_copy');
 
     var url_string = window.location.href;
     var url = new URL(url_string);
@@ -24,21 +15,13 @@ jQuery(document).ready(function () {
         spots.forEach(function (element) {
             spot.append('<option value=' + element.id + '>' + element.name + '</option>');
         });
-        if (sessionStorage.getItem('spotID') == null) {
-            selectSpotByUrlParam();
-            sessionStorage.setItem('spotID', urlparam);
-        } else {
-            loadCache();
-        }
     });
     $.getJSON("assets/report_categories.json", function (categories) {
         categories.forEach(function (element) {
             category.append('<option value=' + element.id + '>' + element.name + '</option>');
         });
-        loadCache();
     });
 
-    updateCache();
     updateLocationLable();
 
     function updateLocationLable() {
@@ -52,11 +35,4 @@ jQuery(document).ready(function () {
         }
     }
 
-    function selectSpotByUrlParam() {
-        if (urlparam != undefined) {
-            spot.val(urlparam);
-            sessionStorage.setItem('spotID', spot.val());
-            sessionStorage.setItem('spotName', $('#selection_bathingspot option:selected').text());
-        }
-    }
 });
