e74f6f816e0ce4869b5ad67480ec74c35d887515
mikioosaki <mikio.osaki@posteo.de>
2018-07-05 04:00:31 +0200

    Specify upload directory

--- server.js
+++ server.js
@@ -44,15 +44,25 @@ app.listen(port, function () {
     console.log('Gulp is running my app on PORT: ' + port);
 });
 
+
+//________________FORMIDABLE______________________
+
+app.use(formidable({
+    encoding: 'utf-8',
+   // uploadDir: '__dirname + '/public/uploads/'',
+    multiples: true, // req.files to be arrays of files
+}));
+
 app.post('/upload', function (req, res) {
-
-        // parse a file upload
-        var form = new formidable.IncomingForm();
- 
-        form.parse(req, function(err, fields, files) {
-          res.writeHead(200, {'content-type': 'text/plain'});
-          res.write('received upload:\n\n');
-          res.end(util.inspect({fields: fields, files: files}));
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.path;
+        var newpath = __dirname + '/public/uploads/' + files.filetoupload.name;
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
         });
-
+    });
 });
